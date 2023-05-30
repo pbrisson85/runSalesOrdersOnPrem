121421aeda53f41b53218ca4c4e6130487afe059
Eastern Dev <dev@seaflow.fish>
2023-05-30 15:14:20 -0400

    initial commit

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ### TODO. Need to manually calc the OTHP (will need a sub table if want the detail as there will be more lines in OTHPO than on the invoice.)
 
 - For now I am not calculating the OTJHP at all.
+
+### Note that through research I have found that the cost on a sales order is based on the average cost of the item at the time the sales order was entered. Within a sales order you can choose tools -> reprice lines to update the average cost as of the current inventory. I NEED TO MANUALLY CALC THIS BASED ON DAILY PERPETUAL
+
+### Note that the cost on an invoice will be the actual lot cost if the line is tagged
---
+++ queries/seasoft/getCatchWeightLines.js
@@ -0,0 +1,44 @@
+const { createConnection } = require('../../database/seasoftODBC')
+const { setFlag, getFlag } = require('../../queries/postgres/flags')
+const logEvent = require('../../queries/postgres/logging')
+
+const getCatchWeightLines = async orders => {
+  try {
+    const errState = await getFlag('odbcErrorState')
+    if (errState) return []
+
+    const odbcConn = await createConnection()
+
+    console.log(`query ODBC for Catch Weight Lines ...`)
+
+    let responses = []
+
+    for (eachOrderLine of orders) {
+      const { ORDER_NUMBER, LOCATION } = eachOrderLine
+      const loc_search = `%${LOCATION}%`
+
+      const queryString = "SELECT {fn RTRIM(\"Catch Weight Lines\".DOCUMENT_LINE_KEY)} AS so_num, {fn RTRIM(\"Catch Weight Lines\".WEIGHT_REC)} AS tagged_array, \"Catch Weight Lines\".QTY_COMMITTED AS qty_committed, {fn RTRIM(\"Catch Weight Lines\".LOCATION_REC)} AS location_array FROM 'Catch Weight Lines' WHERE \"Catch Weight Lines\".DOCUMENT_LINE_KEY = ? and LOCATION_REC LIKE ?" //prettier-ignore
+
+      const response = await odbcConn.query(queryString, [ORDER_NUMBER, loc_search])
+
+      responses.push(response[0])
+    }
+
+    await odbcConn.close()
+
+    return responses
+  } catch (error) {
+    console.error(error)
+
+    setFlag('odbcErrorState', true) // set flag to prevent further requests
+
+    await logEvent({
+      event_type: 'error',
+      funct: 'getCustomerMasterFile',
+      reason: error.message,
+      note: 'flip odbcErrorState flag',
+    })
+  }
+}
+
+module.exports = getCatchWeightLines
