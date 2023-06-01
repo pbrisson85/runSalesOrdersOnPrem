e0f656bfda65863260b29cbb58318bceade86c60
Eastern Dev <dev@seaflow.fish>
2023-06-01 12:12:46 -0400

    initial commit

--- queries/seasoft/getInventoryLocationFile.js
+++ queries/seasoft/getInventoryLocationFile.js
@@ -13,14 +13,16 @@ const getInventoryLocationFile = async catchWeightLines => {
 
     let responses = []
 
-    for (taggedLot of catchWeightLines) {
-      const { lot, loc } = eachOrderLine
+    const keys = Object.keys(catchWeightLines)
+
+    for (key of keys) {
+      const { lot, loc } = catchWeightLines[key]
 
       const queryString = "SELECT {fn RTRIM(\"Inventory Location File\".ITEM_NUMBER)} AS ITEM_NUMBER, {fn RTRIM(\"Inventory Location File\".LOCATION)} AS LOCATION, {fn RTRIM(\"Inventory Location File\".LOT_NUMBER_OR_SIZE)} AS LOT_NUMBER_OR_SIZE, \"Inventory Location File\".LAST_COST FROM 'Inventory Location File' WHERE \"Inventory Location File\".ON_HAND_IN_UM <> 0 AND \"Inventory Location File\".LOCATION = ? AND \"Inventory Location File\".LOT_NUMBER_OR_SIZE = ?" //prettier-ignore
 
       const response = await odbcConn.query(queryString, [loc, lot])
 
-      if (typeof response[0] === 'undefined') console.log('NO INVENTORY FOUND: ', taggedLot) // DEBUG ************************
+      if (typeof response[0] === 'undefined') console.log('NO INVENTORY FOUND: ', catchWeightLines[key]) // DEBUG ************************
 
       responses.push(response[0])
     }
