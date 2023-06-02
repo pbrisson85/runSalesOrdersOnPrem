4b92509f5903c2547a6209cee8f4cdb1436be87c
Eastern Dev <dev@seaflow.fish>
2023-06-02 08:05:21 -0400

    initial commit

--- models/assignCatchWeightLine.js
+++ models/assignCatchWeightLine.js
@@ -7,19 +7,20 @@ const assignCatchWeightLine = (salesOrderLines, nonLotCostedItems) => {
     const { TAGGED_WEIGHT, ITEM_NUMBER, ORDER_NUMBER } = line
 
     if (ix === 0) soNum = ORDER_NUMBER // initialize soNum
+
+    const lotCosted = !nonLotCostedItems.includes(ITEM_NUMBER)
+    const isTagged = TAGGED_WEIGHT > 0 && lotCosted
+
     if (soNum !== ORDER_NUMBER) {
       // reset counter and soNum
       soNum = ORDER_NUMBER
       counter = 0
     } else {
       // increment counter
-      counter++
+      if (isTagged) counter++
     }
 
     // assign counter to line
-
-    const lotCosted = !nonLotCostedItems.includes(ITEM_NUMBER)
-    const isTagged = TAGGED_WEIGHT > 0 && lotCosted
 
     return {
       ...line,
