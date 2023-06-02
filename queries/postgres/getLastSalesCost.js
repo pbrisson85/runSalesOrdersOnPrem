f2a5a0bd4e25f7a6b2d0e8c558d53ee5ba4c57de
Eastern Dev <dev@seaflow.fish>
2023-06-02 12:53:15 -0400

    initial build

--- queries/postgres/getLastSalesCost.js
+++ queries/postgres/getLastSalesCost.js
@@ -1,21 +1,25 @@
 const logEvent = require('./logging')
 
 const getLastSalesCost = async data => {
-  const itemNum = data.line.ITEM_NUMBER
-
   try {
     const { Client } = require('pg')
     const pgClient = new Client() // config from ENV
     await pgClient.connect()
 
-    const response = await pgClient.query(
-      'SELECT (sales_line_items.calc_gl_cogs/sales_line_items.calc_gm_rept_weight) AS cost_lb FROM "salesReporting".sales_line_items WHERE sales_line_items.item_number = $1 ORDER BY sales_line_items.invoice_date DESC LIMIT 1',
-      [itemNum]
-    )
+    let responses = []
+    for (line of data) {
+      const itemNum = line.line.ITEM_NUMBER
+      const response = await pgClient.query(
+        'SELECT (sales_line_items.calc_gl_cogs/sales_line_items.calc_gm_rept_weight) AS cost_lb FROM "salesReporting".sales_line_items WHERE sales_line_items.item_number = $1 ORDER BY sales_line_items.invoice_date DESC LIMIT 1',
+        [itemNum]
+      )
+
+      responses.push({ ...line, lastSale: { costPerLb: response.rows[0].cost_lbs, date: response.rows[0].formatted_invoice_date } })
+    }
 
     await pgClient.end()
 
-    return { ...data, lastSale: { costPerLb: response.rows[0].cost_lbs, date: response.rows[0].formatted_invoice_date } }
+    return responses
   } catch (error) {
     console.log(error)
     await logEvent({
