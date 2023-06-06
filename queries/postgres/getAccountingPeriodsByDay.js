9fb0393cd89e4ee53d1eba47775b1d3c0b354898
Eastern Dev <dev@seaflow.fish>
2023-06-06 09:46:26 -0400

    initial build

--- queries/postgres/getAccountingPeriodsByDay.js
+++ queries/postgres/getAccountingPeriodsByDay.js
@@ -6,7 +6,7 @@ const getAccountingPeriodsByDay = async soDateRange => {
   console.log(`query postgres for accounting periods by day for dates: ${soDateRange.firstDate} through ${soDateRange.lastDate} ...`)
 
   const periodsByDay = await pgClient.query(
-    'SELECT period_by_day.date, period_by_day.period, period_by_day.week, period_by_day.week_serial, period_by_day.period_serial, period_by_day.fiscal_year FROM "accountingPeriods".period_by_day WHERE period_by_day.date >= $1 AND period_by_day.date <= $2',
+    'SELECT period_by_day.date, period_by_day.period, period_by_day.week, period_by_day.week_serial, period_by_day.period_serial, period_by_day.fiscal_year FROM "accountingPeriods".period_by_day WHERE period_by_day.formatted_date >= $1 AND period_by_day.formatted_date <= $2',
     [soDateRange.firstDate, soDateRange.lastDate]
   )
 
