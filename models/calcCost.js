68ee186a087b85cbb69d63f13f4d79983f768bdb
Eastern Dev <dev@seaflow.fish>
2023-06-06 14:41:42 -0400

    initial build

--- models/calcCost.js
+++ models/calcCost.js
@@ -27,7 +27,7 @@ const calcCost = data => {
     // Calc untagged weight ave cost/lb
     let aveUntaggedCost = 0
     if (soline.line.UNTAGGED_WEIGHT !== 0) {
-      if (typeof soline.inventory !== 'undefined') {
+      if (typeof soline.inventory !== 'undefined' && parseInt(soline.inventory.aveOnHandCostPerLb) !== 0) {
         aveUntaggedCost = soline.inventory.aveOnHandCostPerLb
       } else {
         if (typeof soline.lastSalesCost !== 'undefined') {
