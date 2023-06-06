33755c9021ef2e12b04d1d7013bcf49d13a696a6
Eastern Dev <dev@seaflow.fish>
2023-06-06 10:02:04 -0400

    initial build

--- models/getSoDateRange.js
+++ models/getSoDateRange.js
@@ -3,17 +3,20 @@ const getSoDateRange = soHeader => {
     const dateA = new Date(a.SCHEDULED_SHIP_DATE)
     const dateB = new Date(b.SCHEDULED_SHIP_DATE)
 
-    console.log('dateA', dateA)
-    console.log('dateB', dateB)
-    console.log('dateA < dateB', dateA.getTime() < dateB.getTime())
-    console.log('dateA > dateB', dateA.getTime() > dateB.getTime())
-    console.log('dateA === dateB', dateA.getTime() === dateB.getTime())
+    // console.log('dateA', dateA)
+    // console.log('dateB', dateB)
+    // console.log('dateA < dateB', dateA.getTime() < dateB.getTime())
+    // console.log('dateA > dateB', dateA.getTime() > dateB.getTime())
+    // console.log('dateA === dateB', dateA.getTime() === dateB.getTime())
 
     return dateA.getTime() - dateB.getTime()
   })
 
   const firstDate = soHeader[0].SCHEDULED_SHIP_DATE
   const lastDate = soHeader[soHeader.length - 1].SCHEDULED_SHIP_DATE
+
+  console.log('firstDate', firstDate)
+  console.log('lastDate', lastDate)
 
   return { firstDate, lastDate }
 }
