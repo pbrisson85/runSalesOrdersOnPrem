1d54939a12a8f1b8bcb262dadabade530c10fa68
Eastern Dev <dev@seaflow.fish>
2023-06-12 13:28:58 -0400

    trouble shooting null val in ave_untagged_cost for item 5344F010

--- queries/postgres/getLastSalesCost.js
+++ queries/postgres/getLastSalesCost.js
@@ -9,7 +9,7 @@ const getLastSalesCost = async () => {
     console.log(`query postgres to get last sales cost for all items ...`)
 
     const response = await pgClient.query(
-      'SELECT t.item_number, t.formatted_invoice_date, t.calc_gl_cogs/NULLIF(t.calc_gm_rept_weight,0) AS cost_lb FROM (SELECT sales_line_items.item_number, sales_line_items.formatted_invoice_date, sales_line_items.calc_gl_cogs, sales_line_items.calc_gm_rept_weight, row_number() OVER (PARTITION BY sales_line_items.item_number ORDER BY sales_line_items.formatted_invoice_date DESC) AS seqnum FROM "salesReporting".sales_line_items) AS t WHERE seqnum = 1'
+      'SELECT t.item_number, t.formatted_invoice_date, t.calc_gl_cogs/NULLIF(t.calc_gm_rept_weight,0) AS cost_lb FROM (SELECT sales_line_items.item_number, sales_line_items.formatted_invoice_date, sales_line_items.calc_gl_cogs, sales_line_items.calc_gm_rept_weight, row_number() OVER (PARTITION BY sales_line_items.item_number ORDER BY sales_line_items.formatted_invoice_date DESC) AS seqnum WHERE odbc_invoice_number NOT LIKE \'%C\' FROM "salesReporting".sales_line_items) AS t WHERE seqnum = 1'
     )
 
     await pgClient.end()
