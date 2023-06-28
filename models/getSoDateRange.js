97f548683bf7acbab0d51a1bd5f6c314716ca480
Eastern Dev <dev@seaflow.fish>
2023-06-28 07:06:35 -0400

    testing another error in getSoDateRange

--- models/getSoDateRange.js
+++ models/getSoDateRange.js
@@ -6,6 +6,10 @@ const getSoDateRange = soHeader => {
     return dateA.getTime() - dateB.getTime()
   })
 
+  console.log('sorted[0]', sorted[0])
+  console.log('sorted[1]', sorted[1])
+  console.log('sorted[sorted.length - 1]', sorted[sorted.length - 1])
+
   const firstDate = sorted[0].SCHEDULED_SHIP_DATE
   const lastDate = sorted[sorted.length - 1].SCHEDULED_SHIP_DATE
 
