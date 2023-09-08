295243ac5dab4f26b48575bef23513b7f0043e56
Eastern Dev <dev@seaflow.fish>
2023-09-08 13:57:18 -0400

    trying to get tests working

--- tests/testShipToCountryState.js
+++ tests/testShipToCountryState.js
@@ -4,54 +4,56 @@ const sql = require('../server')
 const requestEmailNotification = require('../requests/requestEmail')
 
 const runShipToTests = async () => {
+  const { Client } = require('pg')
+  const pgClient = new Client() // config from ENV
+  await pgClient.connect()
+
   console.log('running ship to country state tests...')
 
   let errors = []
 
   // state should never be blank wih a USA ship to
   console.log('query for blank state')
-  const blankState = await sql`
+  const blankState = await pgClient.query(`
     SELECT so.customer_code, so.customer_name, so.ship_to_code, so.address_source
     FROM "salesReporting".sales_orders AS so
     WHERE (so.state = '' OR so.state IS NULL) AND so.country = 'USA'
     GROUP BY so.customer_code, so.customer_name, so.ship_to_code, so.address_source
-    `
+    `)
 
-  console.log('blankState', blankState)
-
-  if (blankState.length) {
-    let err = `Data testing exception: BLANK STATE found in salesReporting.sales_line_items: ${JSON.stringify(blankState)}`
+  if (blankState.rows.length) {
+    let err = `Data testing exception: BLANK STATE found in salesReporting.sales_line_items: ${JSON.stringify(blankState.rows)}`
     errors.push(err)
     await requestEmailNotification(err)
   }
 
   // country should never be blank
   console.log('query for blank country')
-  const blankCountry = await sql`
+  const blankCountry = await pgClient.query(`
     SELECT so.customer_code, so.customer_name, so.ship_to_code, so.address_source
     FROM "salesReporting".sales_orders AS so
     WHERE so.country = '' OR so.country IS NULL
     GROUP BY so.customer_code, so.customer_name, so.ship_to_code, so.address_source
-    `
+    `)
 
-  if (blankCountry.length) {
-    let err = `Data testing exception: BLANK COUNTRY found in salesReporting.sales_line_items: ${JSON.stringify(blankCountry)}`
+  if (blankCountry.rows.length) {
+    let err = `Data testing exception: BLANK COUNTRY found in salesReporting.sales_line_items: ${JSON.stringify(blankCountry.rows)}`
     errors.push(err)
     await requestEmailNotification(err)
   }
 
   // state should always read 'OUTSIDE USA' with a non USA ship to
   console.log('query for outside usa state')
-  const outSideUsaState = await sql`
+  const outSideUsaState = await pgClient.query(`
     SELECT so.customer_code, so.customer_name, so.ship_to_code, so.address_source
     FROM "salesReporting".sales_orders AS so
     WHERE so.state <> 'OUTSIDE USA' AND so.country <> 'USA'
     GROUP BY so.customer_code, so.customer_name, so.ship_to_code, so.address_source
-    `
+    `)
 
-  if (outSideUsaState.length) {
+  if (outSideUsaState.rows.length) {
     let err = `Data testing exception: missing 'OUTSIDE USA' in state field for foreign country code found in salesReporting.sales_line_items: ${JSON.stringify(
-      outSideUsaState
+      outSideUsaState.rows
     )}`
     errors.push(err)
     await requestEmailNotification(err)
@@ -59,16 +61,16 @@ const runShipToTests = async () => {
 
   // state should never say 'OUTSIDE USA' with a USA ship to
   console.log('query for outside usa country')
-  const outSideUsaCountry = await sql`
+  const outSideUsaCountry = await pgClient.query(`
     SELECT so.customer_code, so.customer_name, so.ship_to_code, so.address_source
     FROM "salesReporting".sales_orders AS so
     WHERE so.state = 'OUTSIDE USA' AND so.country = 'USA'
     GROUP BY so.customer_code, so.customer_name, so.ship_to_code, so.address_source
-    `
+    `)
 
-  if (outSideUsaCountry.length) {
+  if (outSideUsaCountry.rows.length) {
     let err = `Data testing exception: 'OUTSIDE USA' in state field for 'USA' country code found in salesReporting.sales_line_items: ${JSON.stringify(
-      outSideUsaCountry
+      outSideUsaCountry.rows
     )}`
     errors.push(err)
     await requestEmailNotification(err)
