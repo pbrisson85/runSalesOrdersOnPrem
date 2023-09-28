9e1471ea880df7fa98909ab749b18afd8a8dda7b
Eastern Dev <dev@seaflow.fish>
2023-09-28 18:33:27 -0400

    altered connection string trying to make odbc query work around seasoft bugs

--- database/seasoftODBC.js
+++ database/seasoftODBC.js
@@ -5,7 +5,7 @@ const createConnection = async () => {
     'DSN=Seasoft Training; Directory=D:\\Seasoft\\cai_lib; Prefix=D:\\Seasoft_data\\Common Training\\mpd,D:\\Seasoft_data\\Training\\mpd; ViewDLL=D:\\Pxplus; LogFile=\\PXPODBC.LOG; RemotePVKIOHost=10.0.0.253; SERVER=NotTheServer'
 
   const liveDbConnStr =
-    'DSN=Seasoft EFI; Directory=D:\\Seasoft\\cai_lib; Prefix=D:\\Seasoft_data\\Common\\mpd,D:\\Seasoft_data\\Eastern\\mpd; ViewDLL=D:\\Pxplus; LogFile=\\PXPODBC.LOG; RemotePVKIOHost=10.0.0.253; SERVER=NotTheServer'
+    'DSN=Seasoft EFI; Directory=D:\\Seasoft\\cai_lib; Prefix=D:\\Seasoft_data\\Common\\mpd,D:\\Seasoft_data\\Eastern\\mpd; ViewDLL=D:\\Pxplus; LogFile=\\PXPODBC.LOG; RemotePVKIOHost=10.0.0.253; Compression=1; CacheSize=1024; DirtyReads=1; EnforceDouble=1; EnforceNULLDate=1; StripTrailingSpaces=1; ReadOnly=1; SERVER=NotTheServer'
 
   const connection = await odbc.connect(liveDbConnStr)
   return connection
