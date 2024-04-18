70e7443bc791caa38bcdcb5064f4135ef24be9e3
Eastern Dev <dev@seaflow.fish>
2024-04-18 07:19:21 -0400

    commit

--- models/calcOthp.js
+++ models/calcOthp.js
@@ -88,14 +88,15 @@ const calcOthp = (salesOrderLines, othpTable_unflat, othpDefinitions_unflat) => 
       const price = othpPrices[index]
       const cost = price * multiplier
 
-      const contra = othpTable_unflat[code.trim()][0]?.CONTRA ?? 'not found'
+      const contra = othpTable_unflat[code.trim()]?.[0]?.CONTRA ?? 'not found'
       // if contra not listed in contra_sales_gl_map table then ignore
       let ignore = false
       let definition = null
       if (typeof othpDefinitions_unflat[contra] === 'undefined') {
         if (contra === 'not found') {
           // send email notification
-          requestEmailNotification(`othp code: ${code} not found in othp table. Pertaining to sales order: ${ORDER_NUMBER}`)
+          // requestEmailNotification(`othp code: ${code} not found in othp table. Pertaining to sales order: ${ORDER_NUMBER}`)
+          console.log(`othp code: ${code} not found in othp table. Pertaining to sales order: ${ORDER_NUMBER}`)
         }
 
         ignore = true
